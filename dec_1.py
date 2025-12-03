fada81f7e8abfd5e7938c83d71cf439408b28875
fraugabel <katharina.trentau@cloudandheat.com>
2025-12-03 10:34:41 +0100

    format

--- dec_1.py
+++ dec_1.py
@@ -1,40 +1,41 @@
-def dial(position:int ,up:bool, num:int, count:int):
-  print(f"start {position}")
-  if up is True:
-    position +=num
-  else:
-    position -=num
+def dial(position: int, up: bool, num: int, count: int):
+    print(f"start {position}")
+    if up is True:
+        position += num
+    else:
+        position -= num
 
-  print(f"calc {position}")
-  while position< 0 or position > 99:  
-    if position < 0:
-      position=99 - abs(position)
-      count += 1
-  
-    if position > 99:
-      position=position - 99
-      count +=1
+    print(f"calc {position}")
+    while position < 0 or position > 99:
+        if position < 0:
+            position = 99 - abs(position)
+            count += 1
 
-  return position,count
+        if position > 99:
+            position = position - 99
+            count += 1
+
+    return position, count
+
 
 # initiation
-where=50
-password=0
-dial_up=True
-dial_num=0
-file="test_input"
+where = 50
+password = 0
+dial_up = True
+dial_num = 0
+file = "puzzle_input"
 
 with open(file, "r", encoding="utf-8") as f:
     dials = [line.strip() for line in f]
-print(len(dials))    
+print(len(dials))
 
 for d in dials:
-  if d[0] == 'R':
-    dial_up = True
-  else:
-    dial_up = False
-  dial_num=int(d[1:])  
-  where,password=dial(where,dial_up,dial_num,password)
+    if d[0] == "R":
+        dial_up = True
+    else:
+        dial_up = False
+    dial_num = int(d[1:])
+    where, password = dial(where, dial_up, dial_num, password)
 
 print(where)
 print(password)
