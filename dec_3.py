a3266e8de6695c45abd805bb73e47f1a6b525b6b
fraugabel <kathamari@outlook.de>
2025-12-07 18:20:39 +0100

    advent 2

---
+++ dec_3.py
@@ -0,0 +1,3 @@
+# go through the list from the beginnig take each number except the last and compare them to elem1 
+# of a second list with 2 elems and keep the row number as stop  
+# then go through the list from the end until stop and and compare each elemt with elem2
